4972fdf7200d560ae96fb3098c03fbfb897d6611
Pushkar Ambedpelliwar <pushkarna923@gmail.com>
2026-07-30 19:00:38 -0500

    Add ros2_control + gz_ros2_control integration, working diff-drive sim in Gazebo

--- rover_description/launch/gz_sim.launch.py
+++ rover_description/launch/gz_sim.launch.py
@@ -3,6 +3,8 @@ from ament_index_python.packages import get_package_share_directory
 from launch.actions import SetEnvironmentVariable
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from launch_ros.actions import Node
@@ -50,20 +52,57 @@ def generate_launch_description():
     )
 
     # Bridge cmd_vel and odom between ROS 2 and Gazebo transport
-    bridge = Node(
+    #bridge = Node(
+     #   package='ros_gz_bridge',
+      #  executable='parameter_bridge',
+       # arguments=[
+        #    '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
+         #   '/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry',
+        #],
+       # output='screen'
+    #)
+
+    clock_bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
         arguments=[
-            '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
-            '/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry',
+            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'
         ],
         output='screen'
     )
-
+    load_joint_state_broadcaster = RegisterEventHandler(
+        event_handler = OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[
+                Node(
+                    package='controller_manager',
+                    executable='spawner',
+                    arguments=['joint_state_broadcaster'],
+                    output='screen'
+                )
+            ]
+        )
+    )
+    
+    load_diff_drive_controller = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[
+                Node(
+                    package='controller_manager',
+                    executable='spawner',
+                    arguments=['diff_drive_controller'],
+                    output='screen'
+                )
+            ]
+        )
+    )
     return LaunchDescription([
         gz_resource_path,
         robot_state_publisher_node,
         gz_sim,
         spawn_entity,
-        bridge,
+        load_joint_state_broadcaster,
+        load_diff_drive_controller,
+        clock_bridge
     ])
